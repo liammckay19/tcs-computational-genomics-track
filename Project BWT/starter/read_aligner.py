112eea9f7570fe601877c444631ddb944627723f
Liam McKay <ltmckay@ucsc.edu>
2019-09-08 22:36:53 -0700

    removed solutions, added todos

--- Project BWT/starter/read_aligner.py
+++ Project BWT/starter/read_aligner.py
@@ -15,35 +15,9 @@ def find(query, bwt_data):
     results = []
     
     """SOLUTION CODE HERE"""
-    
-    # Start at the end of the query.
-    index = len(query)-1
-    character = query[index] # Looking at the last character in the query.
-    
-    # Initialize the beginning and end variables, establishing
-    # the subset of the sorted suffixes we are currently examining.
-    new_beginning = counts[character] + 1
-    new_end = counts[character] + ranks[character][-1]
-    
+    # TODO: Add code for the beginning of the algorithm
     while index > 0: # We will backtrack through the query until we get to the beginning.
-        
-        old_beginning = new_beginning
-        old_end = new_end
-        
-        character = query[index-1] # Looking at the next character in the query.
-        
-        # Set new beginning and end variables to reflect the new
-        # subset of sorted suffixes we are currently examining.
-        num_in_range = ranks[character][old_end] - ranks[character][old_beginning-1]
-        new_end = counts[character] + ranks[character][old_end]
-        new_beginning = new_end - num_in_range + 1
-        
-        # Return if we can no longer match our query to the reference
-        # at any point in the backtracking process.
-        if num_in_range == 0:
-            return sorted(results)
-        
-        index = index - 1 # Continue backtracking.
+        # TODO: Complete the algorithm for the query
         
     # Add the position(s) in the reference genome to which our
     # query aligns. This information is encoded in the suffix array.
